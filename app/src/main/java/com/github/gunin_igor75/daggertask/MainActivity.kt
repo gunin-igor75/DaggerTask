5d4873de426e1edc81b21271991235741cbb5965
Igor Gunin <gunin-igor75@yandex.ru>
2024-11-05 08:23:42 +0600

    Initial commit

---
+++ app/src/main/java/com/github/gunin_igor75/daggertask/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.github.gunin_igor75.daggertask
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
